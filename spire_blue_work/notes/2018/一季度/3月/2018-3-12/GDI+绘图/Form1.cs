189f46628911eac7eddf29e7b98ebc73fb91d607
2807867265@qq.com <2807867265@qq.com>
2018-03-12 11:10:56 +0800

    2018-3-12 11：10 --commit by adonai --log:GDI+绘图代码片

---
+++ spire_blue_work/notes/2018/一季度/3月/2018-3-12/GDI+绘图/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace GDI绘图
+{
+    /// <summary>
+    /// DrawDemo by GDI
+    /// </summary>
+    public partial class Form1 : Form
+    {
+
+        private static Graphics g;
+        private static GraphicsState gs;
+        private static Pen p;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            g = this.CreateGraphics();
+            p = new Pen(Color.Blue, 2);//定义了一个蓝色,宽度为2的画笔
+        }
+
+        private void drawCircle_Click(object sender, EventArgs e)
+        {
+            gs = g.Save();
+             g.Clear(Color.WhiteSmoke);
+            g.TranslateTransform(20, 70);
+            g.DrawEllipse(p, 0, 0, 200, 200);
+
+            g.Restore(gs);
+
+        }
+
+        private void drawRec_Click(object sender, EventArgs e)
+        {
+            gs = g.Save();
+            g.Clear(Color.WhiteSmoke);
+            int offset = 10;
+            //创建一个数组来存放矩阵的各个坐标
+            PointF[]mypointf=new PointF[] {
+                new PointF(0,0), new PointF(0,200) ,//
+                new PointF(200,200),new PointF(200,0),//
+                new PointF(0,0)};
+            for (int i = 0; i < mypointf.Length; i++)
+			{
+			    mypointf[i].X+=offset;
+                mypointf[i].Y+=offset;
+			}
+            g.DrawLines(p, mypointf);//绘制矩形
+            g.DrawLines(p,new PointF[]{new PointF(300,100),new PointF(250,360)});
+            
+            g.Restore(gs);
+
+        }
+    }
+}
---
+++ spire_blue_work/notes/2018/一季度/3月/2018-3-12/GDI+绘图/PDFDrawDemo.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
+
+namespace GDI绘图
+{
+    /// <summary>
+    /// DrawDemo by PDF API
+    /// </summary>
+    public partial class PDFDrawDemo : Form
+    {
+        //save graphics state
+        private static       PdfGraphicsState state;
+
+
+        public PDFDrawDemo()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// open the process by fileName
+        /// </summary>
+        /// <param name="fileName"></param>
+        private void PDFDocumentViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+
+        private void DrawCircleFunc(PdfPageBase page)
+        {
+
+             state = page.Canvas.Save();
+            
+            PdfPen pen = new PdfPen(Color.DeepSkyBlue, 3);//边框的厚度3
+            PdfBrush brush1 = new PdfSolidBrush(Color.Red);//画刷，颜色为red
+
+            page.Canvas.ScaleTransform(5f, 5f);//缩放操作
+            page.Canvas.TranslateTransform(5f, 1.2f);//平移
+            page.Canvas.DrawEllipse(pen,brush1,0,0,20,20);//drawCircle
+
+            //restor graphics
+            page.Canvas.Restore(state);
+        }
+
+        private void DrawLineFunc(PdfPageBase page)
+        {
+             state = page.Canvas.Save();
+
+            //Draw Rectagle
+            PdfPen pen = PdfPens.DeepSkyBlue;
+            PdfBrush brush = new PdfSolidBrush(Color.Red);//画刷，颜色为red
+             PdfPath path = new PdfPath();
+             float offset = 50;
+             PointF[] points = new PointF[] { new PointF(0, 0), new PointF(0, 80), new PointF(80, 80), new PointF(80, 0) };
+             for (int i = 0; i < points.Length; i++)
+             {
+                 points[i].X += offset;
+                 points[i].Y += offset;
+                 
+             }
+             path.AddLine(points[0],points[1]);
+             path.AddLine(points[1], points[2]);
+             path.AddLine(points[2], points[3]);
+             path.AddLine(points[3], points[0]);
+            
+            page.Canvas.DrawPath(pen, path);
+
+            page.Canvas.TranslateTransform(280, 0);
+            path.FillMode = PdfFillMode.Winding;
+            page.Canvas.DrawPath(pen, brush, path);
+            //drawing line
+            PdfPath Linepath=new PdfPath();
+            Linepath.AddLine(new PointF(70,270),new PointF(96,192));
+            page.Canvas.DrawPath(pen,Linepath);
+
+            //restor graphics
+            page.Canvas.Restore(state);
+        }
+
+
+
+
+        private void drawCircle_Click(object sender, EventArgs e)
+        {
+
+            //Create a pdf document.
+            PdfDocument doc = new PdfDocument();
+            // Create one page
+            PdfPageBase page = doc.Pages.Add();
+            DrawCircleFunc(page);
+            //Save pdf file.
+            doc.SaveToFile("PDFDrawDemo.pdf");
+            doc.Close();
+
+            //Launching the Pdf file.
+            PDFDocumentViewer("PDFDrawDemo.pdf");
+
+        }
+
+        private void drawLine_Click(object sender, EventArgs e)
+        {
+            //Create a pdf document.
+            PdfDocument doc = new PdfDocument();
+
+            // Create one page
+            PdfPageBase page = doc.Pages.Add();
+
+            DrawLineFunc(page);
+
+           
+
+            //Save pdf file.
+            doc.SaveToFile("PDFDrawDemo.pdf");
+            doc.Close();
+
+            //Launching the Pdf file.
+            PDFDocumentViewer("PDFDrawDemo.pdf");
+
+
+        }
+    }
+}
